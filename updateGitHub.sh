3e53f4edb066b7b1eb558375d44e43ede542bbc0
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-09-12 14:54:25 +0300

    modified_updategit_shell_script

--- updateGitHub.sh
+++ updateGitHub.sh
@@ -7,9 +7,9 @@ git add main.py
 git add mySqlDBInterface.py
 git add scheduler.py
 git add updateGitHub.sh
+echo "----Making the commit $1..."
+git commit -m $1
 echo "----Pushing..."
 git push origin master
 # cum rezolv faza cu credentialele...?
-echo "----Making the commit $1..."
-git commit -m $1
 echo "----Git Hub updated succesfully!"
